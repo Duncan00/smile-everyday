fae351fcc01c0ed2f8232a72c2f4d7ba610b23e0
Duncan Mak <kwantakmak@gmail.com>
2015-06-25 00:45:37 +0800

    add floating button service

--- SmileEveryday/app/src/main/java/com/smile/smileeveryday/MainActivity.java
+++ SmileEveryday/app/src/main/java/com/smile/smileeveryday/MainActivity.java
@@ -1,9 +1,12 @@
 package com.smile.smileeveryday;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.smile.smileeveryday.Service.ServiceFloating;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startService(new Intent(MainActivity.this, ServiceFloating.class));
     }
 
 
